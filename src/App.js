10824c8cfbd2015a70209ea419c4445fb90ae6b0
Abdurrofi'i Assajid Arbya <abdurrofii.aas@gmail.com>
2024-09-22 01:17:36 +0700

    Securing API KEY

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=df1c4c69";
+const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
